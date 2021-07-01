fb3e8fc8daad94313c46e4e0e0113e20cd8e0538
Hudson Alcântara <hudson0598@gmail.com>
2021-07-01 15:36:14 -0300

    enviando o modulo 4

---
+++ modulo4_javascript/script.js
@@ -0,0 +1,38 @@
+// Exercício 1: FizzBuzz.
+
+// function fizzBuzz(entrada){
+
+//     if(entrada % 3 == 0 && entrada % 5 == 0){
+//         console.log("FizzBuzz");
+//     } else if(entrada % 3 == 0){
+//         console.log("Fizz");
+//     } else if(entrada % 5 == 0){
+//         console.log("Buzz");
+//     } else if(isNaN(entrada)){
+//         console.log("Não é um número.");
+//     } else {
+//         console.log(entrada);
+//     }
+
+// }
+
+// Exercício 2: Reverse a string.
+
+    // let newStr = '';
+
+    // function reverseAString(str) {
+    //     for(let i = str.length - 1; i >=0; i--){
+    //         newStr += str[i];
+    //     }
+
+    //     console.log(newStr);
+    // }
+
+// Exercício 3: Convert Celsius to Fahrenheit.
+
+    // function converToFahrenheit(value){
+    //     return value * 1.8 + 32
+    // }
+
+
+
